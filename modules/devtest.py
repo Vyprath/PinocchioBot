5e863bba8a9b91ba027e664477f4c02c764a0290
Soham Sen <onerandomghost@gmail.com>
2019-04-06 03:25:21 +0530

    Improved botstats command

--- modules/devtest.py
+++ modules/devtest.py
@@ -1,21 +1,40 @@
 from variables import CMD_POPULARITY
 import database
+import discord
+import psutil
+import os
 from .currency import _add_money
 
 
 async def view_stats(client, message, *args):
-    guilds = client.guilds
     app_info = await client.application_info()
-    await message.channel.send("""
-Stats for this bot: **(Classified Information, kek)**
-**Latency:** {0}
-**Number of Guilds In:** {1}
-**Owner:** {2}
-    """.format(
-        client.latency,
-        len(guilds),
-        app_info.owner.mention
-    ))
+    embed = discord.Embed(
+        title="Bot Stats",
+        description="Running on a VPS with 4GB RAM.")
+    embed.add_field(name="**__General Info__**", inline=False, value="\u200b")
+    embed.add_field(
+        name="Owner",
+        value=f"{app_info.owner.name}#{app_info.owner.discriminator}")
+    embed.add_field(name="Latency", value=f"{client.latency*1000:.03f} ms")
+    embed.add_field(name="Guild Count", value=f"{len(client.guilds):,}")
+    embed.add_field(name="User Count", value=f"{len(client.users):,}")
+    embed.add_field(name="**__Technical Info__**", inline=False, value="\u200b")
+    embed.add_field(name="Overall CPU Usage", value=f"{psutil.cpu_percent()*100:.02f}%")
+    embed.add_field(name="Overall RAM Usage", value=f"{psutil.virtual_memory().percent:.02f}%")
+    this_py = psutil.Process(os.getpid())
+    embed.add_field(name="Bot CPU Usage", value=f"{this_py.cpu_percent()*100:.02f}%")
+    embed.add_field(name="Bot RAM Usage", value=f"{this_py.memory_percent()*100:.02f}%")
+    embed.add_field(name="**__Links__**", inline=False, value="\u200b")
+    embed.add_field(name="Donate", value="[https://patreon.com/RandomGhost](https://patreon.com/RandomGhost)")
+    embed.add_field(name="Website", value="[https://pinocchiobot.tk](https://pinocchiobot.tk)")
+    embed.add_field(name="Discord Bots", value="[https://dbots.pinocchiobot.tk](https://dbots.pinocchiobot.tk)")
+    embed.add_field(name="Support Server", value="[https://support.pinocchiobot.tk](https://support.pinocchiobot.tk)")
+    embed.add_field(name="Invite", value="[https://invite.pinocchiobot.tk](https://invite.pinocchiobot.tk)")
+    embed.add_field(name="Add Waifus", value="[https://waifu.pinocchiobot.tk](https://waifu.pinocchiobot.tk)")
+    embed.set_footer(
+        text="Running on Takagi-VPS • Made by {app_info.owner.name}#{app_info.owner.discriminator}",
+        icon_url=app_info.owner.avatar_url_as(size=128))
+    await message.channel.send(embed=embed)
 
 
 async def famous_cmd(client, message, *args):
@@ -67,7 +86,7 @@ Maybe try contacting the ghost for an upgrade?
 
 
 devtest_functions = {
-    'botstats': (wrapper(view_stats, 3), None),
+    'botstats': (wrapper(view_stats, 0), None),
     'famouscmds': (wrapper(famous_cmd, 4), None),
     'getmoney': (wrapper(get_money, 4), None),
     'repeater': (wrapper(repeater, 5), None),
